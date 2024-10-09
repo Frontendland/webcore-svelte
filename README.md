b86aa7382c755b5393ed60e1745d07dafb49873d
Frontendland <ferenc@frontendland.com>
2024-10-09 09:36:27 +0200

    🎉 Init Webcore Svelte

---
+++ README.md
@@ -0,0 +1,24 @@
+# Webcore Svelte Starter Example
+
+- For more information on how to use Webcore, visit the [documentation](https://webcoreui.dev/).
+- For Svelte integration steps, visit the [Svelte docs](https://webcoreui.dev/docs/svelte).
+
+## Getting Started
+
+Make sure that you have [Node.js](https://nodejs.org/en) installed. Run the following command to install all dependencies:
+
+```
+npm i
+```
+
+After installing dependencies, run the following command to start your dev server:
+
+```
+npm run dev
+```
+
+To build the project, run the following command:
+
+```
+npm run build
+```
---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { Button } from 'webcoreui/svelte'
+    import { github } from 'webcoreui/icons'
+
+    import Layout from './components/Layout.svelte'
+</script>
+
+<Layout>
+	<h1 class="flex center md">
+		Welcome to <img src="logo.svg" width="160" alt="logo" />
+	</h1>
+	<p>
+		Edit <code>src/App.svelte</code> to get started.
+		For documentation, visit <a href="https://webcoreui.dev" target="_blank">webcoreui.dev</a>
+	</p>
+	<div class="flex justify-center xs">
+		<Button href="https://webcoreui.dev" target="_blank">
+			Get Started
+		</Button>
+		<Button
+			href="https://github.com/Frontendland/webcoreui"
+			target="_blank"
+			theme="secondary"
+		>
+			{@html github}
+			GitHub
+		</Button>
+	</div>
+</Layout>
+
+<style lang="scss">
+	@import 'webcoreui/config';
+
+	p {
+		@include typography(center);
+    }
+</style>
